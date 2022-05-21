7e959f208d214bf2e162dfeff7caff0c3118aab9
nakidixon <43101892+nakidixon@users.noreply.github.com>
2022-05-21 04:32:14 -0600

    Test

---
+++ Akurey/res/srs/model/Composition.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Composition {
+    Class class;
+    int endMultiplicity;
+}
+
+
+//setters aplicado a cada variable de la funcion contructor
+public void setClass(String class){this.class = class }
+
+//getters aplicado a cada variable de la funcion contructor
+public String getClass (String class){return class }
